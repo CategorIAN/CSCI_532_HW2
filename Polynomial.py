8e816931d97e9080bd8d20fc20aa4deb59c78917
CategorIAN <ianholm9@gmail.com>
2023-02-03 14:26:07 -0700

    Finished fast multiplication.

--- Polynomial.py
+++ Polynomial.py
@@ -1,9 +1,5 @@
-import numpy as np
-import math
 from functools import reduce
 from cmath import *
-import pandas as pd
-from copy import copy
 
 class Polynomial:
     def __init__(self, coeffs):
@@ -14,53 +10,66 @@ class Polynomial:
         return str(self.coeffs)
 
     def __call__(self, x):
-        return reduce(lambda r, a: r * x + a, self.coeffs[1:], self.coeffs[0])
+        return reduce(lambda r, a: r * x + a, reversed(self.coeffs[:self.deg]), self.coeffs[-1])
+
+    def __mul__(self, Q):
+        n = self.deg + Q.deg + 1
+        (p, q) = (self.get(self.coeffs), self.get(Q.coeffs))
+        return [sum([p(j) * q(i-j) for j in range(self.deg + 1)]) for i in range(n)]
+
+    def get(self, l):
+        def f(i):
+            if i >= 0:
+                try:
+                    return l[i]
+                except:
+                    return 0
+            else:
+                return 0
+        return f
 
     def rou(self, n, k = 1):
         return pow(exp(complex(0, 2 * pi / n)), k)
 
     def even(self):
-        if self.deg % 2 == 0:
-            return Polynomial([self.coeffs[2 * i] for i in range(self.deg // 2 + 1)])
-        else:
-            return Polynomial([self.coeffs[2 * i + 1] for i in range(self.deg // 2 + 1)])
+        return Polynomial([self.coeffs[2 * i] for i in range(self.deg // 2 + 1)])
 
     def odd(self):
-        if self.deg % 2 == 0:
-            return Polynomial([self.coeffs[2 * i + 1] for i in range(self.deg // 2)])
-        else:
-            return Polynomial([self.coeffs[2 * i] for i in range(self.deg // 2 + 1)])
+        return Polynomial([self.coeffs[2 * i + 1] for i in range(self.deg // 2 + int(self.deg % 2 != 0))])
 
-
-    def DFT(self, inv = False):
+    def padded(self, limit = None):
+        limit = 0 if limit is None else limit
         size = 1
         n = self.deg + 1
-        while size < n:
+        while size < n or size < limit:
             size = size * 2
-        padded = copy(self.coeffs)
-        while n < size:
-            padded = [0] + padded
-            n += 1
-        P = Polynomial(padded)
-        return np.vectorize(P.fast_eval(inv))(np.array(range(P.deg + 1)))
+        return Polynomial(self.coeffs + (size - n) * [0])
 
-    def invDFT(self):
-        pass
+    def cround(self, x, d):
+        return complex(round(x.real, d), round(x.imag, d))
 
-    def fast_eval(self, inv, k):
-        n = self.deg
-        def go(P, j):
-            if P.deg < 4:
-                if inv:
-                    return self(self.rou(n, -k)) / n
-                else:
-                    return self(self.rou(n, k))
+    def DFT(self, n = None, inv = False, dec = 2):
+        Q = self.padded(n)
+        n = Q.deg + 1
+        def go(P, k):
+            if P.deg == 0:
+                return [pow(1 / n, int(inv)) * v for v in n * P.coeffs]
             else:
-                return self.even().fast_eval(inv)(2 * k) + self.rou(n, (1 - 2 * int(inv)) * k) * self.odd().fast_eval(2 * k)
-        return f
+                (evenDFT, oddDFT) = (go(P.even(), 2 * k), go(P.odd(), 2 * k))
+                gen = self.rou(n, k)
+                x = 1
+                vals = []
+                for (v_even, v_odd) in zip(evenDFT, oddDFT):
+                    vals = vals + [self.cround(v_even + x * v_odd, dec)]
+                    x = x * gen
+                return vals
+        return go(Q, pow(-1, int(inv)) * 1)
 
-    def fast_multiply(self, Q):
-        fourier_prod = self.DFT() * Q.DFT()
+    def fast_mult(self, Q, dec = 2):
+        n = self.deg + Q.deg + 1
+        prod = [vw[0] * vw[1] for vw in zip(self.DFT(n, dec = dec), Q.DFT(n, dec = dec))]
+        return Polynomial(Polynomial(prod).DFT(inv = True, dec = dec))
+
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from Polynomial import Polynomial
 
 
+def f(i):
+    if i == 1:
+        P = Polynomial([1, 4, 8])
+        print(P.coeffs[-1])
+        print(P.coeffs[P.deg-1:-1:-1])
+        print(P(2))
+    if i == 2:
+        P = Polynomial([2, 4, 3, 9, 7])
+        print(P.even())
+        print(P.odd())
+    if i == 3:
+        P = Polynomial([2, 4, 3, 9, 7])
+        print(P.padded())
+    if i == 4:
+        P = Polynomial([1, 3, 1, 1])
+        print(P.DFT())
+    if i == 5:
+        P = Polynomial([1, 2, 1])
+        Q = Polynomial([-1, 0, 1])
+        print(P.fast_mult(Q, dec = 4))
+    if i == 6:
+        P = Polynomial([1, 1, 2])
+        Q = Polynomial([-1, 1])
+        print(P * Q)
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    P = Polynomial([1, 2, 3, 4, 5, 6, 7, 8])
-    print(P.DFT())
+    f(6)
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
