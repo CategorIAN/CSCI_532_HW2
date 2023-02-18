c34a8548bc7c1f524b6f5addb2a7a747bf523f84
CategorIAN <ianholm9@gmail.com>
2023-02-18 14:04:36 -0700

    Started LeastSquares.

--- DataPoints.py
+++ DataPoints.py
@@ -1,21 +1,38 @@
 import pandas as pd
 import math
 from Line import Line
+import numpy as np
+from functools import reduce
 
 class DataPoints:
     def __init__(self, df):
         self.df = df
-        self.x = df['x']
-        self.y = df['y']
         self.n = df.shape[0]
 
-    def bestLine(self):
-        (xy, xx) = (self.x * self.y, self.x * self.x)
-        a = (self.n * sum(xy) - sum(self.x) * sum(self.y)) / (self.n * sum(xx) - math.pow(sum(self.x), 2))
-        b = (sum(self.y) - a * sum(self.x)) / self.n
+    def bestLine(self, i = 0, j = None):
+        j = None if j is None else self.n
+        (x, y) = (np.array(self.df['x'].iloc[i:j]), np.array(self.df['y'].iloc[i:j]))
+        a = (self.n * x @ y - sum(x) * sum(y)) / (self.n * x @ x - sum(x) * sum(x))
+        b = (sum(y) - a * sum(x)) / self.n
         return Line(a = a, b = b)
 
-    def error(self):
-        L = self.bestLine()
-        fitted = L.fitPoints(self)
-        return sum((self.y - fitted['y']).map(lambda r: math.pow(r, 2)))
+    def error(self, i = 0, j = None):
+        j = None if j is None else self.n
+        if i >= j:
+            return 0
+        else:
+            L = self.bestLine(i, j)
+            fitted = L.fitPoints(self.df['x'].iloc[i:j])
+            resids = np.array(self.df['y'].iloc[i:j] - fitted['y'])
+            return resids @ resids
+
+    def errorMatrix(self):
+        return np.fromfunction(self.error, shape = (self.n, self.n), dtype = float)
+
+    def segmentedLeastSquares(self, cost):
+        em = self.errorMatrix()
+        optError = np.zeros(self.n)
+        for j in range(1, self.n):
+            optError[j] = min(pd.Series(range(j + 1)).map(lambda i: self.error(i, j) + cost + optError[i - 1]))
+
+
--- Line.py
+++ Line.py
@@ -5,8 +5,7 @@ class Line:
         self.a = a
         self.b = b
 
-    def fitPoints(self, pts):
-        x_vals = pts.df['x']
+    def fitPoints(self, x_vals):
         df = pd.DataFrame({'x': x_vals, 'y': x_vals.map(lambda x: self.a * x + self.b)})
         return df
 
